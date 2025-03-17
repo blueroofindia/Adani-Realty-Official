697f8cfeb0fe87256bd7294b0c02a0582e68debd
iaaditya13 <aadityadubey1305@gmail.com>
2025-03-17 23:22:22 +0530

    Vercel comp test 1

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  // Reduce memory usage during development
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 10 * 1000,
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 1,
+  },
+};
+
+module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+      },
       colors: {
         primary: {
           50: '#f0f9f0',
